2dc82c5c4ea53708e3c0a78942a23623a317d4f1
Mikael Arguedas <mikael.arguedas@gmail.com>
2020-02-24 16:15:09 +0100

    revert changes to install_debs_cmake.sh
    Signed-off-by: Mikael Arguedas <mikael.arguedas@gmail.com>

--- scripts/install_debs_cmake.sh
+++ scripts/install_debs_cmake.sh
@@ -24,7 +24,7 @@ sudo apt-get install -y \
     g++ \
     git \
     google-mock \
-    libboost-iostreams-dev \
+    libboost-all-dev \
     libcairo2-dev \
     libeigen3-dev \
     libgflags-dev \
